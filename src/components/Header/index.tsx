50b65fbf02f2e0d81409782e4e17adc2bc0b7837
Timóteo Reinheimer Piano <timoteopiano@gmail.com>
2021-10-14 09:50:29 -0300

    Posts and Preview posts add

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
+import { ActiveLink } from './ActiveLink';
 import { SignInButton } from './SignInButton/index'
 
 import styles from './styles.module.scss';
@@ -6,10 +10,18 @@ export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Link href="/">
+                    <a>
+                        <img src="/images/logo.svg" alt="ig.news" />
+                    </a>
+                </Link>
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a className={styles.active}>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a className={styles.active}>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,59 @@
-import { query as q } from 'faunadb'
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import NextAuth, { Session } from 'next-auth'
+import Providers from 'next-auth/providers'
+import { query as q } from 'faunadb';
 
-import { fauna } from '../../../services/fauna'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      scope: 'read:user'
+      scope: 'read:user',
     }),
-    // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
   callbacks: {
-    async signIn(user, account, profile){
-      const { email } = user
+    async session(session: Session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection(
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                ),
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            )
+          )
+        );
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
+    async signIn(user) {
+      const { email } = user;
 
       try {
         await fauna.query(
@@ -25,7 +62,7 @@ export default NextAuth({
               q.Exists(
                 q.Match(
                   q.Index('user_by_email'),
-                  q.Casefold(user.email)
+                  q.Casefold(email)
                 )
               )
             ),
@@ -36,16 +73,16 @@ export default NextAuth({
             q.Get(
               q.Match(
                 q.Index('user_by_email'),
-                q.Casefold(user.email)
+                q.Casefold(email)
               )
-            )
+            ),
           )
         )
 
-        return true
+        return true;
       } catch {
-        return false
+        return false;
       }
     },
   }
-})
+});
